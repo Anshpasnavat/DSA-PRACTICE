7a2c3c5008d9031bd90a641fd1885f2f3032b200
Ansh <158500596+Anshpasnavat@users.noreply.github.com>
2026-01-21 20:02:40 +0530

    Daily DSA practice

--- code.cpp
+++ code.cpp
@@ -1,17 +1,20 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
  int main(){
     int arr[]= {1,2,3,4,5};
     int n = sizeof(arr)/sizeof(int);
+    int maxSum = INT_MIN;
 
     for(int i = 0; i<n; i++){
         for(int j =i;j<n;j++){
-            for (int k = i; k <= j; k++) {
-                cout << arr[k];
+            int currSum = 0;
+            for (int k = i; k <= j; k++){
+                currSum  += arr[k];
             }
-            cout <<",";
+            maxSum = max(maxSum, currSum);
+            
         }
-        cout << endl;    
+         
     }
-    return 0;
+    cout << maxSum ; 
 }
