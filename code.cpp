b77ebf99547874b27f704bbbb38e0de4d4329080
Ansh <158500596+Anshpasnavat@users.noreply.github.com>
2026-07-23 22:14:30 +0530

    Number of Unique XOR Triplets I

--- code.cpp
+++ code.cpp
@@ -1,17 +1,24 @@
 #include<iostream>
 using namespace std;
- int main(){
-    int arr[]= {-1,1,2,-2,1};
+int majorityElement(int arr[], int n){
+    for(int i=0;i<n;i++){
+        int count = 0;
+        for(int j= 0;j<n;j++){
+            if(arr[i]==arr[j]){
+                count++;
+            }
+        }if(count > n/2){
+            return arr[i];
+        }
+    }
+    return -1;
+
+}
+ 
+
+int main(){
+    int arr[]= {2,1,1};
     int n = sizeof(arr)/sizeof(int);
     int sum = 0;
-
-    for(int i = 0; i<n; i++){
-        for(int j =i;j<n;j++){
-            for (int k = j; k < n; k++) {
-                sum += arr[i] + arr[j] + arr[k];
-            }
-        }    
-    }
-    cout << sum;
-    return 0;
+    cout  << majorityElement(arr,n);
 }
