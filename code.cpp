8e9949010b77ed82fff9ff9b27838d4d3e1a2890
Ansh <158500596+Anshpasnavat@users.noreply.github.com>
2026-07-28 19:38:02 +0530

    Search in Rotated Sorted Array

--- code.cpp
+++ code.cpp
@@ -1,26 +1,35 @@
 #include<iostream>
-using  namespace std;
+#include<utility>
+using namespace std;
 
-int lowerBound(int arr[], int n, int target){
-    int ans = n;
-    int left = 0;
-    int right = n-1;
-    while(left<=right){
-        int mid = left+(right-left)/2;
-        if(arr[mid]>=target){
-            ans = mid;
-            right = mid-1;
-        }else{
-            left = mid + 1;
-        }
-
+pair<int,int> floorCeil(int arr[], int n, int target){
+    // find ceil: smallest >= target
+    int left = 0, right = n-1;
+    int ceilVal = -1;
+    while(left <= right){
+        int mid = left + (right-left)/2;
+        if(arr[mid] >= target){
+            ceilVal = arr[mid];
+            right = mid - 1;
+        } else left = mid + 1;
     }
-    return ans;
+    // find floor: largest <= target
+    left = 0; right = n-1;
+    int floorVal = -1;
+    while(left <= right){
+        int mid = left + (right-left)/2;
+        if(arr[mid] <= target){
+            floorVal = arr[mid];
+            left = mid + 1;
+        } else right = mid - 1;
+    }
+    return {floorVal, ceilVal};
 }
 
-int main (){
-    int arr[] = {1,2,3,4,5};
+int main(){
+    int arr[] = {2,4,6,8,10};
     int n = sizeof(arr)/sizeof(int);
-    int target = 5;
-    cout << lowerBound(arr, n, target);
+    int target = 12;
+    auto res = floorCeil(arr, n, target);
+    cout << "floor=" << res.first << " ceil=" << res.second;
 }
