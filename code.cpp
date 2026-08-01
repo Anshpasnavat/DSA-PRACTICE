52f8c10dc3e484a6a55de5defb0f039ccaa30770
Ansh <158500596+Anshpasnavat@users.noreply.github.com>
2026-08-01 20:00:19 +0530

    Predict the Winner

--- code.cpp
+++ code.cpp
@@ -1,27 +1,35 @@
-#include<iostream>
+#include <iostream>
+#include <algorithm>
 #include<climits>
 using namespace std;
 
-int lastOccurence(int arr[], int n, int target){
+int maxSumSubarray(int arr[], int n, int target)
+{
+    int currSum= 0;
     int left = 0;
-    int right = n-1;
-    while(left<=right){
-        int mid = left+(right-left)/2;
-        if(arr[mid]<=target){
-            left = mid +1; 
-        }else{
-            right = mid - 1;
+    int ans = INT_MAX;
+
+    for(int right=0; right<n;right++){
+        currSum += arr[right];
+        while(currSum >= target){
+            int length = right - left +1;
+            ans = min(ans, length) ;
+            currSum -= arr[left];
+            left ++;
         }
-    }if(right>=0&& arr[right] == target ){
-        return right;
-    }else{
-        return -1;
-    }
+    }if(ans == INT_MAX){
+    return 0;
+}else{
+    return ans;
+}
 }
 
-int main(){
-    int arr[] = {1,3,3,3,4,4,5,5,5,5,7,9};
-    int n  = sizeof(arr)/sizeof(int);
-    int target = 3;
-    cout << lastOccurence(arr , n , target);
+
+int main() {
+
+    int arr[] = {2,1,5,1,3,2};
+    int n = sizeof(arr) / sizeof(int);
+    int target = 9;
+    cout << maxSumSubarray(arr, n, target);
+
 }
